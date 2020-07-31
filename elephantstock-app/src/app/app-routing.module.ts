407ea9539c840a7e402725b7e914a7917c090b0f
devninja9 <devninja9@gmail.com>
2020-07-31 23:41:18 +0800

    Add page not found

--- elephantstock-app/src/app/app-routing.module.ts
+++ elephantstock-app/src/app/app-routing.module.ts
@@ -4,13 +4,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { UserCreateComponent } from './components/user-create/user-create.component';
 import { UserEditComponent } from './components/user-edit/user-edit.component';
 import { UserListComponent } from './components/user-list/user-list.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
-import { from } from 'rxjs';
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'user-list' },
   { path: 'create-user', component: UserCreateComponent },
   { path: 'edit-user/:id', component: UserEditComponent },
   { path: 'user-list', component: UserListComponent },
+  { path: '**', component: PageNotFoundComponent },
 ];
 
 @NgModule({
--- elephantstock-app/src/app/app.module.ts
+++ elephantstock-app/src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+
 import { ReactiveFormsModule } from '@angular/forms';
 import { NgxSpinnerModule } from 'ngx-spinner';
 import { ToastrModule } from 'ngx-toastr';
 
 import { ApiService } from './service/api.service';
+import { HttpErrorInterceptor } from './service/http-error.interceptor';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserCreateComponent } from './components/user-create/user-create.component';
 import { UserEditComponent } from './components/user-edit/user-edit.component';
 import { UserListComponent } from './components/user-list/user-list.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
@@ -20,6 +23,7 @@ import { UserListComponent } from './components/user-list/user-list.component';
     UserCreateComponent,
     UserEditComponent,
     UserListComponent,
+    PageNotFoundComponent,
   ],
   imports: [
     BrowserModule,
@@ -35,7 +39,10 @@ import { UserListComponent } from './components/user-list/user-list.component';
       closeButton: false,
     }),
   ],
-  providers: [ApiService],
+  providers: [
+    ApiService,
+    { provide: HTTP_INTERCEPTORS, useClass: HttpErrorInterceptor, multi: true },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
